7fac34f3fcb34a71fb8b0314abd168a812435dc1
Khushi_soni <khushisoni.hipl@gmail.com>
2023-09-14 18:04:28 +0530

    Set basic navbar in react app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import {Button} from 'react-bootstrap'
+import {Button} from 'react-bootstrap';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Header from './Header';
+import Login from './Login';
+import Register from './Register';
+import AddProduct from './AddProduct';
+import UpdateProduct from './UpdateProduct';
 
 function App() {
   return (
     <div className="App">
-      <h1>React Project</h1>
-      <button>Normal btn</button>
-      <Button>Bootstrap btn</Button>
+      <BrowserRouter>
+      <Header />
+     
+      <Routes>
+      <Route path="/login" element={<Login/>}>Login</Route>
+      <Route path="/register" element={<Register/>}>Login</Route>
+      <Route path="/add" element={<AddProduct/>}>Login</Route>
+      <Route path="/update" element={<UpdateProduct/>}>Login</Route>
+      </Routes>
+      
+      </BrowserRouter>
     </div>
   );
 }
